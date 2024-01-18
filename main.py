8b57cbc007874a3f4ee538e13ef4b6aeee0ab876
Alexis <daloy5alx@gmail.com>
2024-01-18 18:35:44 -0400

    inicializando

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def hallar_divisores(n):
+	print(f"Los divisores de {n} son:", end=" ")
+	for i in range(1, n + 1):
+		if n % i == 0:
+			print(i, end=" ")
+	print()
+
+def main():
+	print("Programa para hallar los divisores de un numero")
+	while True:
+		entrada = int(input("por favor inserte un numero: "))
+		hallar_divisores(entrada)
+
+if __name__ == "__main__":
+	main()
